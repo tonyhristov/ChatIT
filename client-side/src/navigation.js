b331c911c0348a053c1adf94fddbc302ecd0c067
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-08-04 14:45:29 +0300

    feat: add about page

--- client-side/src/navigation.js
+++ client-side/src/navigation.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/home";
+import AboutPage from "./pages/about";
 
-const Navigation = ()=>{
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={HomePage} />
-            </Switch>
-        </BrowserRouter>
-    )
-}
+const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-export default Navigation
+export default Navigation;
