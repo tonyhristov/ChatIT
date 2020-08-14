74548e10b4e390f83d6040687bd457c26e71f76f
Antonio <47687247+tonyhristov@users.noreply.github.com>
2020-08-15 01:30:31 +0300

    fix: messages

--- server-side/controllers/message.js
+++ server-side/controllers/message.js
@@ -3,11 +3,8 @@ const models = require("../models");
 module.exports = {
   getMessages: (req, res, next) => {
     models.Message.find()
-      // .populate("author")
       .then((messages) => {
         const filtered = messages.filter((messages) => {
-          // return messages.chat == id;
-
           const author = req.headers.referer;
 
           return messages.chat == author.split("/").pop();
@@ -22,8 +19,6 @@ module.exports = {
     const { message, chatId } = req.body;
     const { _id, username } = req.user;
     const id = req._id;
-
-    console.log(req.user);
 
     models.Message.create({
       message,
